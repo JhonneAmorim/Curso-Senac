df11d6a52944695edf7108318f5dbc04e28773b8
JhonneAmorim <jhonneamorimdev@gmail.com>
2022-10-17 20:35:32 -0300

    exercicio

---
+++ reajuste_salarial/app.py
@@ -0,0 +1,8 @@
+from reajuste_salarial import  ReajusteSalarial
+
+calculo_do_reajuste = ReajusteSalarial(699, 0.15)
+
+calculo_do_reajuste.mostrar_salario()
+calculo_do_reajuste.mostrar_reajuste()
+calculo_do_reajuste.calcular_reajuste()
+calculo_do_reajuste.mostrar_aumento()
