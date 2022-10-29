cd7e2b5e258dadb48c3298078cbc84d2662faf78
JhonneAmorim <jhonneamorimdev@gmail.com>
2022-10-29 16:45:31 -0300

    exercicio 2 de função

---
+++ curso_python/funcoes/exercicio_2.py
@@ -0,0 +1,7 @@
+# 2 - Crie uma função que recebe 3 números como parâmentros e exiba a soma enra eles.
+
+def somar_numeros(x, y, z):
+  return x + y + z
+
+somando = somar_numeros(14, 15, 11)
+print(somando)
